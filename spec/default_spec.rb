88fa11f1ea652ea0bd9a1e2b14028c2e07618c5c
Björn Albers <bjoernalbers@googlemail.com>
2013-03-19 12:57:54 +0100

    Remove unused node.json (replaced by default attributes)

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -4,7 +4,6 @@ describe 'osirix::default' do
   let(:chef_run) { ChefSpec::ChefRunner.new.converge 'osirix::default' }
 
   it 'downloads OsiriX' do
-    #chef_run.should create_remote_file('/tmp/OsiriX.zip').with(
     expect(chef_run).to create_remote_file('/tmp/OsiriX.zip').with(
       :source => 'http://www.osirix-viewer.com/OldVersions/OsiriX4.1.2.pkg-Int.zip',
       :checksum => 'ff831f8fa55fffec94e98c26af998756591b3cbb255ba15372e07807af2ac6cc'
