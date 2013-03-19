001ae3b074fc3c2a955bf222a63f60e9b0433201
Björn Albers <bjoernalbers@googlemail.com>
2013-03-19 12:56:47 +0100

    Switch to new rspec syntax.

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -4,21 +4,22 @@ describe 'osirix::default' do
   let(:chef_run) { ChefSpec::ChefRunner.new.converge 'osirix::default' }
 
   it 'downloads OsiriX' do
-    chef_run.should create_remote_file('/tmp/OsiriX.zip').with(
+    #chef_run.should create_remote_file('/tmp/OsiriX.zip').with(
+    expect(chef_run).to create_remote_file('/tmp/OsiriX.zip').with(
       :source => 'http://www.osirix-viewer.com/OldVersions/OsiriX4.1.2.pkg-Int.zip',
       :checksum => 'ff831f8fa55fffec94e98c26af998756591b3cbb255ba15372e07807af2ac6cc'
     )
   end
 
   it 'extracts OsiriX' do
-    chef_run.should execute_command('unzip -o /tmp/OsiriX.zip').with(
+    expect(chef_run).to execute_command('unzip -o /tmp/OsiriX.zip').with(
       :cwd => '/tmp',
       :creates => '/tmp/OsiriX.pkg'
     )
   end
 
   it 'installs OsiriX' do
-    chef_run.should execute_command('sudo installer -pkg /tmp/OsiriX.pkg -target /').with(
+    expect(chef_run).to execute_command('sudo installer -pkg /tmp/OsiriX.pkg -target /').with(
       :creates => '/Applications/OsiriX.app'
     )
   end
